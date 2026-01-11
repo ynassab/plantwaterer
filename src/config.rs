ec72bd2c1b4436b7fb5470917e486204cdceaeda
Yahia Nassab <33182857+ynassab@users.noreply.github.com>
2026-01-11 10:54:40 -0500

    Updated state file location

--- src/config.rs
+++ src/config.rs
@@ -4,8 +4,8 @@ pub const WATER_PUMP_PIN: u8 = 27;
 pub const MIXING_TIME_SECS: u64 = 30;
 pub const WATERING_TIME_SECS: u64 = 12;
 
-pub const STATE_DIR_PATH: &str = "/var/lib/plant-watering";
-pub const STATE_FILE_NAME: &str = "/cycle_state.json";
+pub const STATE_DIR_PATH: &str = ".";
+pub const STATE_FILE_NAME: &str = "cycle_state.json";
 
 pub const LOG_LEVEL: &str = "info";
 
