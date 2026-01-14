d225f592779da655891546a04e548a0f030eddd4
Yahia Nassab <33182857+ynassab@users.noreply.github.com>
2026-01-13 22:04:01 -0500

    Removed unused logging constant

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,4 @@ pub const WATERING_TIME_SECS: u64 = 12;
 pub const STATE_DIR_PATH: &str = ".";
 pub const STATE_FILE_NAME: &str = "cycle_state.json";
 
-pub const LOG_LEVEL: &str = "info";
-
 pub const SIGNAL_HANDLER_CHECK_INTERVAL_SECONDS: u64 = 1;
